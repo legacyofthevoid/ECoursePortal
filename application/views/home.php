74d66183887b2d22abb1dc756f8a303befe3a4a0
Carrie <carrieanne.reyes@gmail.com>
2013-10-12 21:16:16 +0800

    edited home view

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,40 @@
-this is the home page
+<html>
+	<head>
+		<title>Home-Online Course Portal</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="ocpstyles.css"/>
+	</head>
+	
+	<body>
+		<div id="header" class="headerstyle">
+			<span>
+				<img src="<?php echo base_url();?>images/e-course.png" alt="Course Portal" class="homelogo"/>
+			</span>
+			<span class="homefont">
+				ECourse Portal
+			</span>
+			<span class="bread">
+				<a href = "login.html" class="hyperlink">Logout</a> 
+			</span>
+			<span class="bread">
+				<a href = "registration.html" class="hyperlink">Account Settings</a>
+			</span>
+			<span class="bread">
+				<a href = "registration.html" class="hyperlink">Home</a>
+			</span>
+		</div>
+		<div class="divleft">
+			<span>
+				<img src="<?php echo base_url();?>images/aaron.jpg" alt="Aaron Espallardo" class="userpic"/>
+			</span>
+			<span>
+				Hello, Aaron!
+			</span>
+			<ul class="nav nav-list">
+				<li class="nav-header">Course</li>
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#">Library</a></li>
+			</ul>
+		<div>
+	</body>
+</html>
